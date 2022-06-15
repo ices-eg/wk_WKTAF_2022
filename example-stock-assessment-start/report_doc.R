2bd2be76eb5b03bad23567f316e020f8a89646eb
Colin Millar <colin.millar@ices.dk>
2022-06-15 14:27:16 +0200

    report

--- example-stock-assessment-start/report_doc.R
+++ example-stock-assessment-start/report_doc.R
@@ -7,6 +7,7 @@ mkdir("report")
 # combine into a word document
 render("report.Rmd",
   output_file = "report.html",
+#  output_file = "report.docx",
   encoding = "UTF-8"
 )
 # cp("report.docx", "report", move = TRUE)
