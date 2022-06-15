2ff7880d88e00d09791adde7d5f52d9ac05ad4d0
Colin Millar <colin.millar@ices.dk>
2022-06-15 13:33:46 +0200

    updates

--- example-stock-assessment-start/output.R
+++ example-stock-assessment-start/output.R
@@ -25,28 +25,28 @@ partab <- partable(fit)
 
 # Fs
 fatage <- faytable(fit)
-fatage <- fatage[, 2:8]
+fatage <- fatage[, -1]
 fatage <- as.data.frame(fatage)
 
 # Ns
 natage <- as.data.frame(ntable(fit))
 
 # Catch
-catab <- catchtable(fit)
+catab <- as.data.frame(catchtable(fit))
 colnames(catab) <- c("Catch", "Low", "High")
 
 # TSB
-tsb <- tsbtable(fit)
+tsb <- as.data.frame(tsbtable(fit))
 colnames(tsb) <- c("TSB", "Low", "High")
 
 # Summary Table
-tab.summary <- cbind(summary(fit), tsb)
+tab.summary <- cbind(as.data.frame(summary(fit)), tsb)
+tab.summary <- cbind(tab.summary, rbind(catab, NA))
 colnames(tab.summary)[1] <- "R_a0"
 colnames(tab.summary)[7] <- "Fbar_a3_5"
-summary <- as.data.frame(tab.summary)
 
 ## Write tables to output directory
 write.taf(
-  c("partab", "summary", "natage", "fatage"),
+  c("partab", "tab.summary", "natage", "fatage"),
   dir = "output"
 )
