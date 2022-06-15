9e65b7cce47f7b1ce6c2ea2aae6cbaa51e12e988
Colin Millar <colin.millar@ices.dk>
2022-06-15 13:16:16 +0200

    update

--- example-stock-assessment-start/bootstrap/sam_fit.R
+++ example-stock-assessment-start/bootstrap/sam_fit.R
@@ -4,5 +4,8 @@ library(stockassessment)
 # download model from stockassessment.org
 fit <- fitfromweb("WBCod_2021_cand01")
 
+retro_fit <- stockassessment::retro(fit, year = 2019:2021)
+
 # save to model folder
 save(fit, file = "fit.RData")
+save(retro_fit, file = "retro_fit.RData")
