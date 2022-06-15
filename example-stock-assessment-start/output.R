a6fe42ca8dcbc5749455d7e4bdd3f82deb31cd88
Colin Millar <colin.millar@ices.dk>
2022-06-15 13:23:44 +0200

    update

--- example-stock-assessment-start/output.R
+++ example-stock-assessment-start/output.R
@@ -7,3 +7,46 @@ library(icesTAF)
 
 mkdir("output")
 
+## Extract results of interest, write TAF output tables
+
+## Before:
+## After: csv tables of assessment output
+
+library(icesTAF)
+library(stockassessment)
+
+mkdir("output")
+
+# load fit
+(load("model/fit.rData"))
+
+# Model Parameters
+partab <- partable(fit)
+
+# Fs
+fatage <- faytable(fit)
+fatage <- fatage[, 2:8]
+fatage <- as.data.frame(fatage)
+
+# Ns
+natage <- as.data.frame(ntable(fit))
+
+# Catch
+catab <- catchtable(fit)
+colnames(catab) <- c("Catch", "Low", "High")
+
+# TSB
+tsb <- tsbtable(fit)
+colnames(tsb) <- c("TSB", "Low", "High")
+
+# Summary Table
+tab.summary <- cbind(summary(fit), tsb)
+colnames(tab.summary)[1] <- "R_a0"
+colnames(tab.summary)[7] <- "Fbar_a3_5"
+summary <- as.data.frame(tab.summary)
+
+## Write tables to output directory
+write.taf(
+  c("partab", "summary", "natage", "fatage"),
+  dir = "output"
+)
