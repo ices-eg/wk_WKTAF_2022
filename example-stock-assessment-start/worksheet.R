9dad8e8ccb03222216efea40f34d71864cacf5c3
Colin Millar <colin.millar@ices.dk>
2022-06-15 11:16:04 +0200

    update

--- example-stock-assessment-start/worksheet.R
+++ example-stock-assessment-start/worksheet.R
@@ -1,9 +1,21 @@
 
 install.packages("remotes")
 
-#remotes::install_github("fishfollower/sam/stockassessment")
+# remotes::install_github("fishfollower/sam/stockassessment")
+# remotes::install_github("fishfollower/SAM/stockassessment", INSTALL_opts=c("--no-multiarch"))
+
+# because this is a special package we need to add it to
+# the taf analysis
 
 library(icesTAF)
+
+# add any special packages (almost always github ones)
+draft.software("stockassessment")
+draft.software("stockassessment", file = TRUE)
+
+taf.bootstrap(software = TRUE)
+
+library(stockassessment)
 library(rmarkdown)
 
 taf.bootstrap()
