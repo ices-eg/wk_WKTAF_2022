48150942735d6afa008f573d0b79d03aa66f6d68
Colin Millar <colin.millar@ices.dk>
2022-06-15 15:51:20 +0200

    comments

--- example-stock-assessment-start/report_doc.R
+++ example-stock-assessment-start/report_doc.R
@@ -4,11 +4,12 @@ source("utilities.R")
 
 mkdir("report")
 
-# combine into a word document
+# combine into a word and html document
 render("report.Rmd",
   output_format = c("word_document", "html_document"),
   encoding = "UTF-8"
 )
 
+# move to report folder
 cp("report.html", "report", move = TRUE)
 cp("report.docx", "report", move = TRUE)
